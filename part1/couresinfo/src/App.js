ba75ad72109b844755ceb8b366b0c1a6418a9073
bagwuel <mbagwu39@gmail.com>
2023-06-01 15:27:00 +0100

    exercise 1.3

---
+++ part1/couresinfo/src/App.js
@@ -0,0 +1,53 @@
+const Header = ({course}) => <h1>{course}</h1>
+
+const Part = ({part, exercises}) => <p>{part} {exercises}</p>
+
+const Content = ({
+  part1, part2, part3, 
+exercises1, exercises2, exercises3
+}) => (
+<div>
+  <Part part={part1} exercises={exercises1} />
+  <Part part={part2} exercises={exercises2} />
+  <Part part={part3} exercises={exercises3} />    
+</div>
+)
+const Total = ({exercises1, exercises2, exercises3}) => <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content 
+        part1={part1.name}
+        exercises1={part1.exercises}
+        part2={part2.name}
+        exercises2={part2.exercises}
+        part3={part3.name}
+        exercises3={part3.exercises}
+        />
+      <Total 
+      exercises1={part1.exercises}
+      exercises2={part2.exercises}
+      exercises3={part3.exercises}
+      />
+    </div>
+  )
+}
+
+export default App
